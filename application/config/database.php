d613dc810631c71d2b29ae069e17de6a00a4873f
caiquemart <caiquemart.cs@gmail.com>
2016-06-28 19:07:38 -0300

    Arquivos de configuração

--- application/config/database.php
+++ application/config/database.php
@@ -78,7 +78,7 @@ $db['default'] = array(
 	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '',
-	'database' => 'click_pesquisa_db',
+	'database' => 'clickpesquisa',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
@@ -94,3 +94,28 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
+
+$db['clickdecisao'] = array(
+	'dsn'	=> '',
+	'hostname' => 'clicktimes.com.br',
+	'username' => 'root',
+	'password' => 'Physis_2013',
+	'database' => 'clickdecisao',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+
+
